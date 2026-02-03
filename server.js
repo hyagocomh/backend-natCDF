31760a03c723f804667555aa4316ecd6b32a6fb8
hyagocomh <hya.az.le@gmail.com>
2026-02-03 00:47:38 -0300

    attCupons2

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ const preference = new Preference(client);
    CUPONS
 ================================ */
 const CUPONS = {
-  ALUNONAT3: { curso: 'MAX NATCDF (Combo Completo)', valorFinal: 360 },
-  ALUNONAT2: { curso: 'NATCDF Combo 2 Matérias', valorFinal: 270 },
-  ALUNONAT1: { curso: 'NATCDF 1 Matéria', valorFinal: 160 }
+  ALUNO3: { curso: 'MAX NATCDF (Combo Completo)', valorFinal: 360 },
+  ALUNO2: { curso: 'NATCDF Combo 2 Matérias', valorFinal: 270 },
+  ALUNO1: { curso: 'NATCDF 1 Matéria', valorFinal: 160 }
 };
 
 /* ===============================
